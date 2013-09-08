31de913adde21010e66f19b35658c376f65aa845
bahui80 <bahui80@hotmail.com>
2013-09-08 18:43:02 -0300

    CSS y create.sql

---
+++ Buitter/src/test/resources/create.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE Users (
+	userid SERIAL PRIMARY KEY NOT NULL,  
+	name VARCHAR(32) NOT NULL,
+	surname VARCHAR(32) NOT NULL,
+	password VARCHAR(32) NOT NULL,
+	date DATE NOT NULL,
+	photo BYTEA,
+	username VARCHAR(32) NOT NULL	
+);
+
+CREATE TABLE Buits (
+	buitid SERIAL PRIMARY KEY NOT NULL,  
+	message VARCHAR(500) NOT NULL,
+	userid INTEGER NOT NULL REFERENCES Users(userid),
+	date DATE NOT NULL
+);
+
+CREATE TABLE Hashtags (
+	hashtag VARCHAR(40) NOT NULL,
+	hashtagid SERIAL PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE Buithash (
+	buitid INTEGER NOT NULL,
+	hashtagid INTEGER NOT NULL,
+	PRIMARY KEY(buitid, hashtagid)
+);
