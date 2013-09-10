73b250504aba75152387fb145c190d8c3345d83d
Facundo Menzella <facumenzella@gmail.com>
2013-09-10 16:27:51 -0300

    "Usermanager date changes"

--- Buitter/src/main/java/it/itba/edu/ar/dao/UserManager.java
+++ Buitter/src/main/java/it/itba/edu/ar/dao/UserManager.java
@@ -5,9 +5,11 @@ import it.itba.edu.ar.connection.DatabaseException;
 import it.itba.edu.ar.model.User;
 
 import java.sql.Connection;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 
 public class UserManager implements UserDao {
 
@@ -24,7 +26,18 @@ public class UserManager implements UserDao {
 		
 		manager.register(new User("Juan", "Buireo", "jujubuireo", "1234", 
 			"Sexo,drogas, rocanrol", "Como se llama mi perro?", "Boris", 
-			"12-10-2013", null));
+			new Date(), null));
+		
+//		manager.updateUser(new User("Juan", "Buireo", "jujubuireo", "1234", 
+//				"Vivir al limite", "Como se llama mi perro?", "Boris", 
+//				new Date(0), null));
+//		
+//		System.out.println(manager.checkUserName("jujubuireo"));;
+//		
+//		manager.changePassword(new User("Juan", "Buireo", "jujubuireo", "12345", 
+//				"Vivir al limite", "Como se llama mi perro?", "Boris", 
+//				new Date(0), null));
+//		
 	}
 	
 	public static synchronized UserManager sharedInstance(){
@@ -39,7 +52,20 @@ public class UserManager implements UserDao {
 	}
 	
 	public boolean checkUserName(String username) {
-		// TODO Auto-generated method stub
+		try {
+			Connection connection = manager.getConnection();
+			PreparedStatement stmt = connection.prepareStatement(
+					"SELECT * FROM Users WHERE username = ?");
+			stmt.setString(1,username);
+
+			ResultSet results = stmt.executeQuery();
+			if (results.next()) {
+				return true;
+			}
+			connection.close();
+		} catch (SQLException e) {
+			throw new DatabaseException(e.getMessage(), e);
+		}
 		return false;
 	}
 
@@ -47,7 +73,8 @@ public class UserManager implements UserDao {
 		User usr = null;
 		try {
 			Connection connection = manager.getConnection();
-			PreparedStatement stmt = connection.prepareStatement("SELECT . FROM Users WHERE username = ? AND password = ?");
+			PreparedStatement stmt = connection.prepareStatement(
+					"SELECT . FROM Users WHERE username = ? AND password = ?");
 			stmt.setString(1, user.getUsername());
 			stmt.setString(2,user.getPassword());
 
@@ -55,7 +82,7 @@ public class UserManager implements UserDao {
 			if (results.next()) {
 				usr = new User(results.getNString(2),results.getNString(3),results.getNString(7),
 						results.getNString(4),results.getNString(8),results.getNString(9),
-						results.getNString(10),	results.getNString(11),results.getNString(12));
+						results.getNString(10),	results.getDate(11),results.getNString(12));
 			}
 			connection.close();
 		} catch (SQLException e) {
@@ -68,19 +95,20 @@ public class UserManager implements UserDao {
 		try {
 			Connection connection = manager.getConnection();
 			PreparedStatement stmt = connection.prepareStatement
-					("INSERT INTO Users VALUES(?,?,?,?,?,?,?,?,?,?)");
+					("INSERT INTO Users(name,surname,password," +
+							"date,username,description,secret_question," +
+							"secret_answer) VALUES(?,?,?,?,?,?,?,?)");
+			
 			stmt.setString(1, user.getName());
 			stmt.setString(2,user.getSurname());
 			stmt.setString(3,user.getPassword());
-			stmt.setString(4,user.getCreationDate());
-			stmt.setString(5,user.getCreationDate());
-			stmt.setString(6, null);
-			stmt.setString(7, user.getUsername());
-			stmt.setString(8, user.getDescription());
-			stmt.setString(9, user.getSecretQuestion());
-			stmt.setString(10, user.getSecretAnswer());
-			
-			stmt.executeQuery();
+			stmt.setDate(4,new java.sql.Date(user.getCreationDate().getTime()));
+			stmt.setString(5, user.getUsername());
+			stmt.setString(6, user.getDescription());
+			stmt.setString(7, user.getSecretQuestion());
+			stmt.setString(8, user.getSecretAnswer());
+
+			stmt.execute();
 
 			connection.close();
 		} catch (SQLException e) {
@@ -93,21 +121,20 @@ public class UserManager implements UserDao {
 		try {
 			Connection connection = manager.getConnection();
 			PreparedStatement stmt = connection.prepareStatement
-					("UPDATE Users SET VALUES(?,?,?,?,?,?,?,?,?,?)" +
-							"WHERE username = ?");
+					("UPDATE Users SET name =  ?, surname = ?, password = ?," +
+							"description = ?, secret_question = ?, secret_answer = ?" +
+							" WHERE username = ?" );
+			
 			stmt.setString(1, user.getName());
 			stmt.setString(2,user.getSurname());
 			stmt.setString(3,user.getPassword());
-			stmt.setString(4,user.getCreationDate());
-			stmt.setString(5,user.getCreationDate());
-			stmt.setString(6, null);
-			stmt.setString(7, user.getUsername());
-			stmt.setString(8, user.getDescription());
-			stmt.setString(9, user.getSecretQuestion());
-			stmt.setString(10, user.getSecretAnswer());
-			stmt.setString(11, user.getUsername());
+			stmt.setString(4, user.getDescription());
+			stmt.setString(5, user.getSecretQuestion());
+			stmt.setString(6, user.getSecretAnswer());
 			
-			stmt.executeQuery();
+			stmt.setString(7, user.getUsername());
+			
+			stmt.execute();
 
 			connection.close();
 		} catch (SQLException e) {
