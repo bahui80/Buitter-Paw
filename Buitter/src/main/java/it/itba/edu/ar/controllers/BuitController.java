4377d51e2c4b5b8a2dc42928cf27780b5d88c924
Facundo Menzella <facumenzella@gmail.com>
2013-10-14 15:29:46 -0300

    Converters, falta probarlo pero estan creados

--- Buitter/src/main/java/it/itba/edu/ar/controllers/BuitController.java
+++ Buitter/src/main/java/it/itba/edu/ar/controllers/BuitController.java
@@ -13,7 +13,6 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +20,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
