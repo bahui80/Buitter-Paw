b5fdd8d3b2d29c888aeb56836d4658e781d53c24
Facundo Menzella <facumenzella@gmail.com>
2013-10-31 10:51:18 -0300

    ahi creo que va

---
+++ Buitter/src/main/java/it/itba/edu/ar/model/ReBuitEvent.java
@@ -0,0 +1,12 @@
+package it.itba.edu.ar.model;
+
+import java.util.Date;
+
+public class ReBuitEvent extends Event {
+
+	public ReBuitEvent(Date date, User user) {
+		super(date, user);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
