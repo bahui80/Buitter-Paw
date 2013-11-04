4a7dc16d24eb4a40b72584098955331c5f203802
bahui80 <bahui80@hotmail.com>
2013-11-04 04:23:09 -0300

    form hecho perfcto y andando

--- Buitter/src/main/java/it/itba/edu/ar/web/UserController.java
+++ Buitter/src/main/java/it/itba/edu/ar/web/UserController.java
@@ -3,13 +3,15 @@ package it.itba.edu.ar.web;
 import it.itba.edu.ar.model.Buit;
 import it.itba.edu.ar.model.User;
 import it.itba.edu.ar.repo.UserRepo;
+import it.itba.edu.ar.web.validator.UserForm;
+import it.itba.edu.ar.web.validator.UserFormValidator;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
@@ -17,11 +19,10 @@ import java.util.TreeMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.fileupload.DiskFileUpload;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
+import org.hibernate.HibernateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,11 +35,12 @@ import org.springframework.web.servlet.ModelAndView;
 public class UserController {
 
 	private UserRepo userRepo;
-	private Boolean error = false;
-
+	private UserFormValidator validator;
+	
 	@Autowired
-	public UserController(UserRepo userRepo) {
+	public UserController(UserRepo userRepo, UserFormValidator validator) {
 		this.userRepo = userRepo;
+		this.validator = validator;
 	}
 
 	/*
@@ -87,7 +89,8 @@ public class UserController {
 			@RequestParam(value = "answer", required = false) String answer,
 			@RequestParam(value = "password", required = false) String password,
 			@RequestParam(value = "password2", required = false) String password2) {
-
+		
+		boolean error = false;
 		ModelAndView mav = new ModelAndView();
 
 		if (state.equals("recover")) {
@@ -106,11 +109,7 @@ public class UserController {
 				error = true;
 				mav.addObject("error_answer", "Incorrect secret answer");
 			}
-			RequestAttributes requestAttributes = RequestContextHolder
-					.getRequestAttributes();
-			HttpServletRequest req = ((ServletRequestAttributes) requestAttributes)
-					.getRequest();
-			checkPassword(password, password2, req);
+			error = checkPassword(password, password2, mav);
 			if (error) {
 				error = false;
 			} else {
@@ -160,130 +159,55 @@ public class UserController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView editprofile(
-			@RequestParam(value = "a", required = false) String a,
-			HttpSession session) {
-		ModelAndView mav = new ModelAndView();
-		RequestAttributes requestAttributes = RequestContextHolder
-				.getRequestAttributes();
-		HttpServletRequest request = ((ServletRequestAttributes) requestAttributes)
-				.getRequest();
-
-		DiskFileUpload fu = new DiskFileUpload();
-		List<FileItem> fileItems;
-
-		try {
-			fileItems = fu.parseRequest(request);
-		} catch (FileUploadException e) {
-			throw new ServletValidationException();
-		}
-
-		String username = fileItems.get(0).getString();
-		String password = fileItems.get(1).getString();
-		String password2 = fileItems.get(2).getString();
-		String name = fileItems.get(3).getString();
-		String surname = fileItems.get(4).getString();
-		String description = fileItems.get(5).getString();
-		String question = fileItems.get(6).getString();
-		String answer = fileItems.get(7).getString();
-		boolean privacy = fileItems.get(8).getString().equals("Private") ? true
-				: false;
-		String photoName = fileItems.get(9).getName();
-		byte[] photo = null;
-
-		if (checkPhoto(photoName, mav)) {
-			photo = fileItems.get(9).get();
-		}
-
-		checkPassword(password, password2, request);
-		checkName(name, request);
-		checkSurname(surname, request);
-		checkDescription(description, request);
-		request.setAttribute("user_question", question);
-		request.setAttribute("user_privacy", privacy);
-		checkAnswer(answer, question, request);
-
-		if (error) {
-			error = false;
-			request.setAttribute("user_username", username);
-			request.setAttribute("action", "editprofile");
-			mav.setViewName("user/registration");
+	public ModelAndView editprofile(UserForm userForm, Errors errors, HttpSession session) {
+		ModelAndView mav = new ModelAndView("user/registration");
+		
+		mav.addObject("action", "editprofile");
+		validator.validate(userForm, errors);
+		
+		if(errors.hasErrors()) {
 			return mav;
 		}
-
-		User user = userRepo.get(username);
-
-		if (photo == null) {
-			photo = user.getPhoto();
+		
+		try {
+			userForm.update(userRepo.get((String) session.getAttribute("user")));
+		} catch (IOException e) {
+			errors.rejectValue("photo", "unopen");
+			return mav;
 		}
-
-		user.setPassword(password);
-		user.setName(name);
-		user.setSurname(surname);
-		user.setDescription(description);
-		user.setSecretQuestion(question);
-		user.setSecretAnswer(answer);
-		user.setPrivacy(privacy);
-		user.setPhoto(photo);
-
+		
 		mav.setViewName("redirect:../home/home");
 		return mav;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView registration(
-			@RequestParam(value = "a", required = false) String a,
-			HttpSession session) {
+	public ModelAndView registration(UserForm userForm, Errors errors, HttpSession session) {
 		ModelAndView mav = new ModelAndView();
-		RequestAttributes requestAttributes = RequestContextHolder
-				.getRequestAttributes();
-		HttpServletRequest request = ((ServletRequestAttributes) requestAttributes)
-				.getRequest();
-		DiskFileUpload fu = new DiskFileUpload();
-		List<FileItem> fileItems;
-
-		try {
-			fileItems = fu.parseRequest(request);
-		} catch (FileUploadException e) {
-			throw new ServletValidationException();
-		}
-
-		String username = fileItems.get(0).getString();
-		String password = fileItems.get(1).getString();
-		String password2 = fileItems.get(2).getString();
-		String name = fileItems.get(3).getString();
-		String surname = fileItems.get(4).getString();
-		String description = fileItems.get(5).getString();
-		String question = fileItems.get(6).getString();
-		String answer = fileItems.get(7).getString();
-		boolean privacy = fileItems.get(8).getString().equals("Private") ? true
-				: false;
-		String photoName = fileItems.get(9).getName();
-		byte[] photo = null;
-
-		if (checkPhoto(photoName, mav)) {
-			photo = fileItems.get(9).get();
-		}
-
-		checkUsername(username, request);
-		checkPassword(password, password2, request);
-		checkName(name, request);
-		checkSurname(surname, request);
-		checkDescription(description, request);
-		request.setAttribute("user_question", question);
-		request.setAttribute("user_privacy", privacy);
-		checkAnswer(answer, question, request);
+		User user;
 		mav.addObject("action", "registration");
-		if (error) {
-			error = false;
-			return mav;
-		} else {
-			userRepo.add(new User(name, surname, username, password,
-					description, question, answer, new Date(), 0, privacy,
-					photo));
-			session.setAttribute("user", username);
+		validator.validate(userForm, errors);
+		
+		if(errors.hasErrors()) {
+			return mav; 
 		}
-		return new ModelAndView("redirect:../home/home");
+		
+		
+		try {
+			user = userForm.build();
+		} catch (IOException e1) {
+			errors.rejectValue("photo", "unopen");
+			return mav;
+		}
+			
+		try {
+			userRepo.add(user);
+		} catch (HibernateException e) {
+			errors.rejectValue("username", "repeated");
+			return mav;
+		}
+		session.setAttribute("user", userForm.getUsername());
+		mav.setViewName("redirect:../home/home");
+		return mav;	
 	}
 
 	/*
@@ -316,20 +240,9 @@ public class UserController {
 		ModelAndView mav = new ModelAndView();
 
 		String username = (String) session.getAttribute("user");
-		mav.addObject("action", "editprofile");
 		if (username != null) {
 			User user = userRepo.get(username);
-
-			mav.addObject("user_username", user.getUsername());
-			mav.addObject("user_password", user.getPassword());
-			mav.addObject("user_password2", user.getPassword());
-			mav.addObject("user_name", user.getName());
-			mav.addObject("user_surname", user.getSurname());
-			mav.addObject("user_description", user.getDescription());
-			mav.addObject("user_question", user.getSecretQuestion());
-			mav.addObject("user_answer", user.getSecretAnswer());
-			mav.addObject("user_privacy", user.getPrivacy());
-
+			mav.addObject("userForm", new UserForm(user));
 			mav.setViewName("user/registration");
 		} else {
 			mav.setViewName("error");
@@ -359,6 +272,7 @@ public class UserController {
 		}
 
 		mav.addObject("action", "registration");
+		mav.addObject("userForm", new UserForm());
 
 		return mav;
 	}
@@ -366,153 +280,32 @@ public class UserController {
 	/*
 	 * OTHER METHODS
 	 */
-	private void checkUsername(String username, HttpServletRequest request) {
-		if (!username.equals("")) {
-			if (username.trim().length() == 0
-					|| !username.matches("[a-zA-Z0-9]+")) {
-				request.setAttribute("error_username",
-						"Invalid format username");
-				error = true;
-			}
-			if (username.length() <= 32) {
-				// if (userService.checkUsername(username)) {
-				// request.setAttribute("error_username",
-				// "The username already exists");
-				// error = true;
-				// }
-			} else {
-				request.setAttribute("error_username",
-						"The username must contain up to 32 characters");
-				error = true;
-			}
-			request.setAttribute("user_username", username);
-		} else {
-
-			request.setAttribute("error_username",
-					"You must insert an username");
-			error = true;
-		}
-	}
-
-	private void checkPassword(String password, String password2,
-			HttpServletRequest request) {
+	
+	private boolean checkPassword(String password, String password2, ModelAndView mav) {
 		if (!password.equals("")) {
-			if (!password.matches("[a-zA-Z0-9]+")) {
-				request.setAttribute("error_password",
+			if (password.trim().length() == 0 || !password.matches("[a-zA-Z0-9]+")) {
+				mav.addObject("error_password",
 						"Invalid password format");
-				error = true;
+				return true;
 			}
 			if (password.length() > 32) {
-				request.setAttribute("error_password",
+				mav.addObject("error_password",
 						"The password must contain up to 32 characters");
-				error = true;
-			} else if (!password2.equals(password)) {
-				request.setAttribute("error_password2",
-						"The passwords must match");
-				error = true;
-			}
-			request.setAttribute("user_password2", password2);
-			request.setAttribute("user_password", password);
-		} else {
-			request.setAttribute("error_password", "You must insert a password");
-			error = true;
-		}
-	}
-
-	private void checkName(String name, HttpServletRequest request) {
-		if (!name.equals("")) {
-			if (name.trim().length() == 0 || !name.matches("^[\\p{L} ]+$")) {
-				request.setAttribute("error_name", "Invalid format name");
-				error = true;
-			}
-			if (name.length() > 32) {
-				request.setAttribute("error_name",
-						"The name must contain up to 32 characters");
-				error = true;
-			}
-			request.setAttribute("user_name", name);
-		} else {
-			request.setAttribute("error_name", "You must insert a name");
-			error = true;
-		}
-	}
-
-	private void checkSurname(String surname, HttpServletRequest request) {
-		if (!surname.equals("")) {
-			if (surname.trim().length() == 0
-					|| !surname.matches("^[\\p{L} ]+$")) {
-				request.setAttribute("error_surname", "Invalid format surname");
-				error = true;
-			}
-			if (surname.length() > 32) {
-				request.setAttribute("error_surname",
-						"The surname must contain up to 32 characters");
-				error = true;
-			}
-			request.setAttribute("user_surname", surname);
-		} else {
-			request.setAttribute("error_surname", "You must insert a surname");
-			error = true;
-		}
-	}
-
-	private void checkDescription(String description, HttpServletRequest request) {
-		if (!description.equals("")) {
-			if (description.trim().length() == 0) {
-				request.setAttribute("error_description",
-						"Invalid format description");
-				error = true;
-			}
-			if (description.length() > 140) {
-				request.setAttribute("error_description",
-						"The description must contain up to 140 characters");
-				error = true;
-			}
-			request.setAttribute("user_description", description);
-		} else {
-			request.setAttribute("error_description",
-					"You must insert a description");
-			error = true;
-		}
-	}
-
-	private void checkAnswer(String answer, String question,
-			HttpServletRequest request) {
-		if (!answer.equals("")) {
-			if (answer.trim().length() == 0) {
-				request.setAttribute("error_answer", "Invalid format answer");
-				error = true;
-			}
-			if (answer.length() > 60) {
-				request.setAttribute("error_answer",
-						"The answer must contain up to 60 characters");
-				error = true;
-			}
-			request.setAttribute("user_answer", answer);
-
-		} else {
-			request.setAttribute("error_answer",
-					"You must insert a secret answer");
-			error = true;
-		}
-		request.setAttribute("question", question);
-	}
-
-	private boolean checkPhoto(String photoName, ModelAndView mav) {
-		if (!photoName.equals("")) {
-			if (photoName.toLowerCase().contains("jpg")
-					|| photoName.toLowerCase().contains("jpeg")
-					|| photoName.toLowerCase().contains("png")) {
 				return true;
-			} else {
-				mav.addObject("error_photo",
-						"File invalid. Only jpeg, jpg or png images");
-				error = true;
+			} else if (!password2.equals(password)) {
+				mav.addObject("error_password2",
+						"The passwords must match");
+				return true;
 			}
+			mav.addObject("user_password2", password2);
+			mav.addObject("user_password", password);
+		} else {
+			mav.addObject("error_password", "You must insert a password");
+			return true;
 		}
 		return false;
 	}
-
+	
 	private Map<String, Integer> groupedData(Set<Buit> buits, String groupby) {
 		Map<String,Integer> data = null;
 		if (groupby.equals("month")) {
--- Buitter/src/main/webapp/WEB-INF/jsp/user/registration.jsp
+++ Buitter/src/main/webapp/WEB-INF/jsp/user/registration.jsp
@@ -10,106 +10,99 @@
 <div class="row row-offcanvas row-offcanvas-right">
 <div class="col-md-8 col-md-offset-2">
     <div class="well">
-              <form class="bs-example form-horizontal" method="post" action="<c:out value="${action}"/>" enctype="multipart/form-data">
+              <form:form class="bs-example form-horizontal" method="POST" commandName="userForm" action="${action}" enctype="multipart/form-data">
                 <fieldset>
                   <legend>Your personal details</legend>
                   <c:if test="${action == 'editprofile'}">
-                      <input type="hidden" class="form-control2" name="username" value="<c:out value="${user_username}"/>"/>
+                      <form:input type="hidden" class="form-control2" name="username" path="username" value="${user_username}"/>
                   </c:if>
                   <c:if test="${action == 'registration'}">
+                    <c:set var="error_username"><form:errors path="username"/></c:set>
                     <div class="form-group <c:if test="${not empty error_username}">has-error</c:if>">
                       <label for="username" class="col-md-3 control-label">Username</label>
                       <div class="col-md-8">
-                        <input type="text" class="form-control2" maxlength="32" placeholder="Username" name="username" value="<c:out value="${user_username}"/>"/>
-                        <c:if test="${not empty error_username}">
-                          <p class="text-danger" style="margin-bottom: 0px"><small><em><c:out value="${error_username}"/></em></small></p>
-                        </c:if>
+                        <form:input type="text" class="form-control2" maxlength="32" placeholder="Username" name="username" path="username" value="${user_username}"/>
+                        <p class="text-danger" style="margin-bottom: 0px"><small><em><form:errors path="username" /></em></small></p>
                       </div>
                     </div>
                   </c:if>
+                  <c:set var="error_password"><form:errors path="password"/></c:set>
                   <div class="form-group <c:if test="${not empty error_password}">has-error</c:if>">
                     <label for="password" class="col-md-3 control-label">Password</label>
                     <div class="col-md-8">
-                      <input type="password" maxlength="32" class="form-control2" placeholder="Password" name="password" value="<c:out value="${user_password}"/>"/>
-                      <c:if test="${not empty error_password}">
-                        <p class="text-danger" style="margin-bottom: 0px"><small><em><c:out value="${error_password}"/></em></small></p>
-                      </c:if>
+                      <form:input type="password" maxlength="32" class="form-control2" placeholder="Password" name="password" path="password" value="${user_password}"/>
+                        <p class="text-danger" style="margin-bottom: 0px"><small><em><form:errors path="password" /></em></small></p>
                     </div>
                   </div>
+                   <c:set var="error_password2"><form:errors path="password2"/></c:set>
                    <div class="form-group <c:if test="${not empty error_password2}">has-error</c:if>">
                     <label for="re-password" class="col-md-3 control-label">Re-type password</label>
                     <div class="col-md-8">
-                      <input type="password" maxlength="32" class="form-control2" placeholder="Re-type password" name="password2" value="<c:out value="${user_password2}"/>"/>
-                      <c:if test="${not empty error_password2}">
-                        <p class="text-danger" style="margin-bottom: 0px"><small><em><c:out value="${error_password2}"/></em></small></p>
-                      </c:if>
+                      <form:input type="password" maxlength="32" class="form-control2" placeholder="Re-type password" name="password2" path="password2" value="${user_password2}"/>
+                      <p class="text-danger" style="margin-bottom: 0px"><small><em><form:errors path="password2" /></em></small></p>
                     </div>
                   </div>
+                  <c:set var="error_name"><form:errors path="name"/></c:set>
                   <div class="form-group <c:if test="${not empty error_name}">has-error</c:if>">
                     <label for="name" class="col-md-3 control-label pull-left">Name</label>
                     <div class="col-md-8">
-                      <input type="text" class="form-control2" maxlength="32" placeholder="Name" name="name" value="<c:out value="${user_name}"/>"/>
-                      <c:if test="${not empty error_name}">
-                        <p class="text-danger" style="margin-bottom: 0px"><small><em><c:out value="${error_name}"/></em></small></p>
-                      </c:if>
+                      <form:input type="text" class="form-control2" maxlength="32" placeholder="Name" name="name" path="name" value="${user_name}"/>
+                      <p class="text-danger" style="margin-bottom: 0px"><small><em><form:errors path="name" /></em></small></p>
                     </div>
                   </div>
+                  <c:set var="error_surname"><form:errors path="surname"/></c:set>
                   <div class="form-group <c:if test="${not empty error_surname}">has-error</c:if>">
                     <label for="surname" class="col-md-3 control-label">Surname</label>
                     <div class="col-md-8">
-                      <input type="text" class="form-control2" maxlength="32" placeholder="Surname" name="surname" value="<c:out value="${user_surname}"/>"/>
-                      <c:if test="${not empty error_surname}">
-                        <p class="text-danger" style="margin-bottom: 0px"><small><em><c:out value="${error_surname}"/></em></small></p>
-                      </c:if>
+                      <form:input type="text" class="form-control2" maxlength="32" placeholder="Surname" name="surname" path="surname" value="${user_surname}"/>
+                      <p class="text-danger" style="margin-bottom: 0px"><small><em><form:errors path="surname" /></em></small></p>
                     </div>
                   </div>
+                    <c:set var="error_description"><form:errors path="description"/></c:set>
                     <div class="form-group <c:if test="${not empty error_description}">has-error</c:if>">
                     <label for="description" class="col-md-3 control-label">Description</label>
                     <div class="col-md-8">
-                      <textarea class="form-control2" rows="4" maxlength="140" name="description"><c:out value="${user_description}"/></textarea>
+                      <form:textarea class="form-control2" rows="4" maxlength="140" name="description" path="description"/>
+                      <c:out value="${user_description}"/>
                       <span class="help-block">Write a description for others users to get to know you</span>
-                      <c:if test="${not empty error_description}">
-                        <p class="text-danger" style="margin-bottom: 0px"><small><em><c:out value="${error_description}"/></em></small></p>
-                      </c:if>
+                      <p class="text-danger" style="margin-bottom: 0px"><small><em><form:errors path="description" /></em></small></p>
                     </div>
                   </div>
                   <div class="form-group">
                     <label for="question" class="col-md-3 control-label">Secret question</label>
                     <div class="col-md-8">
-                      <select class="form-control2" name="question">
-                        <option <c:if test="${user_question == 'What is the name of your dog?'}">selected="selected"</c:if> >What is the name of your dog?</option>
-                        <option <c:if test="${user_question == 'Who was your favourite teacher?'}">selected="selected"</c:if> >Who was your favourite teacher?</option>
-                        <option <c:if test="${user_question == 'Where do you live?'}">selected="selected"</c:if> >Where do you live?</option>
-                        <option <c:if test="${user_question == 'Do you hate Twitter?'}">selected="selected"</c:if> >Do you hate Twitter?</option>
-                        <option <c:if test="${user_question == 'What is your aunts name?'}">selected="selected"</c:if> >What is your aunts name?</option>
-                      </select>
+                      <form:select class="form-control2" name="question" path="question">
+                          <form:option value="What is the name of your dog?">What is the name of your dog?</form:option>
+                          <form:option value="Who was your favourite teacher?">Who was your favourite teacher?</form:option>
+                          <form:option value="Where do you live?">Where do you live?</form:option>
+                          <form:option value="Do you hate Twitter?">Do you hate Twitter?</form:option>
+                          <form:option value="What is your aunts name?">What is your aunts name?</form:option>
+                      </form:select>
                     </div>
                   </div>
+                  <c:set var="error_answer"><form:errors path="answer"/></c:set>
                   <div class="form-group <c:if test="${not empty error_answer}">has-error</c:if>">
                     <label for="answer" class="col-md-3 control-label">Secret answer</label>
                     <div class="col-md-8">
-                      <input type="text" maxlength="60" class="form-control2" placeholder="Type your answer" name="answer" value="<c:out value="${user_answer}"/>"/>
-                      <c:if test="${not empty error_answer}">
-                        <p class="text-danger" style="margin-bottom: 0px"><small><em><c:out value="${error_answer}"/></em></small></p>
-                      </c:if>
+                      <form:input type="text" maxlength="60" class="form-control2" placeholder="Type your answer" name="answer" path="answer" value="${user_answer}"/>
+                      <p class="text-danger" style="margin-bottom: 0px"><small><em><form:errors path="answer" /></em></small></p>
                     </div>
                   </div>
                   <div class="form-group">
                     <label for="privacy" class="col-md-3 control-label">Privacy</label>
                     <div class="col-md-8">
-                      <select class="form-control2" name="privacy">
-                        <option <c:if test="${user_privacy == false}">selected="selected"</c:if> >Public</option>
-                        <option <c:if test="${user_privacy == true}">selected="selected"</c:if> >Private</option>
-                      </select>
+                      <form:select class="form-control2" name="privacy" path="privacy">
+                        <form:option value="Public">Public</form:option>
+                        <form:option value="Private">Private</form:option>
+                     </form:select>
                     </div>
                   </div>
+                  <c:set var="error_photo"><form:errors path="photo"/></c:set>
                   <div class="form-group <c:if test="${not empty error_photo}">has-error</c:if>">
                     <label for="photo" class="col-md-3 control-label">Profile picture</label>
                     <div class="col-md-8">
-                      <input type="file" data-buttonText="Select photo" accept="image/jpeg, image/jpg, image/png" name="photo"/>
-                       <c:if test="${not empty error_photo}">
-                        <p class="text-danger" style="margin-bottom: 0px"><small><em><c:out value="${error_photo}"/></em></small></p>
-                      </c:if>
+                      <form:input type="file" data-buttonText="Select photo" accept="image/jpeg, image/jpg, image/png" name="photo" path="photo"/>
+                      <p class="text-danger" style="margin-bottom: 0px"><small><em><form:errors path="photo" /></em></small></p>
                     </div>
                   </div>
                   <div class="form-group">
@@ -119,7 +112,7 @@
                     </div>
                   </div>
                 </fieldset>
-              </form>
+              </form:form>
             </div>
           </div>
         </div>
