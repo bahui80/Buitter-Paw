ac78bf4a784c2706cbf5e344d41bf35401a8cd64
bahui80 <bahui80@hotmail.com>
2014-01-24 01:27:09 -0300

    hecho el basepage. Faltan agregar los links de la barra

---
+++ Buitter/src/main/java/it/itba/edu/ar/web/HomePage.java
@@ -0,0 +1,9 @@
+package it.itba.edu.ar.web;
+
+import it.itba.edu.ar.web.base.BasePage;
+
+public class HomePage extends BasePage {
+	public HomePage() {
+		
+	}
+}
--- Buitter/src/main/java/it/itba/edu/ar/web/base/BasePage.java
+++ Buitter/src/main/java/it/itba/edu/ar/web/base/BasePage.java
@@ -1,10 +1,27 @@
 package it.itba.edu.ar.web.base;
 
+import it.itba.edu.ar.web.BuitterSession;
+import it.itba.edu.ar.web.HomePage;
+
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
 public class BasePage extends WebPage {
 
 	public BasePage() {
-		
+		add(new BookmarkablePageLink<Void>("homePageLink", HomePage.class));
+		WebMarkupContainer loggedContainer = new WebMarkupContainer("logged") {
+			public boolean isVisible() {
+				return BuitterSession.get().isSignedIn();
+			}
+		};
+		WebMarkupContainer notLoggedContainer = new WebMarkupContainer("notLogged") {
+			public boolean isVisible() {
+				return !BuitterSession.get().isSignedIn();
+			}
+		};
+		add(loggedContainer);
+		add(notLoggedContainer);
 	}
 }
