9c13b85a53d6aae9f500e40c027733e14b198d67
juan <bahui80@gmail.com>
2014-08-07 23:06:30 -0300

    comienzo de followings y followers

--- Buitter/src/main/java/it/itba/edu/ar/web/buit/FollowersPage.java
+++ Buitter/src/main/java/it/itba/edu/ar/web/buit/FollowersPage.java
@@ -1,12 +1,28 @@
 package it.itba.edu.ar.web.buit;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-
+import it.itba.edu.ar.domain.user.User;
+import it.itba.edu.ar.domain.user.UserRepo;
 import it.itba.edu.ar.web.base.BasePage;
 
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
 public class FollowersPage extends BasePage {
+	@SpringBean
+	private UserRepo userRepo;
 	
 	public FollowersPage(final PageParameters pgParameters) {
+		final IModel<User> modelUser = new LoadableDetachableModel<User>() {
+			@Override
+			protected User load() {
+				return userRepo.get(pgParameters.get("username").toString());
+			}
+		};
+		
+		add(new HeaderPanel("headerPanel", modelUser));
+		
 		
 	}
 }
--- Buitter/src/main/java/it/itba/edu/ar/web/buit/FollowingPage.java
+++ Buitter/src/main/java/it/itba/edu/ar/web/buit/FollowingPage.java
@@ -1,12 +1,26 @@
 package it.itba.edu.ar.web.buit;
 
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import it.itba.edu.ar.domain.user.User;
+import it.itba.edu.ar.domain.user.UserRepo;
 import it.itba.edu.ar.web.base.BasePage;
 
 public class FollowingPage extends BasePage {
+	@SpringBean
+	private UserRepo userRepo;
 	
 	public FollowingPage(final PageParameters pgParameters) {
+		final IModel<User> modelUser = new LoadableDetachableModel<User>() {
+			@Override
+			protected User load() {
+				return userRepo.get(pgParameters.get("username").toString());
+			}
+		};
 		
+		add(new HeaderPanel("headerPanel", modelUser));
 	}
 }
