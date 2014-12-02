65288b83d97d250040d79f13e59028ee88db1f29
juan <bahui80@gmail.com>
2014-12-02 00:58:12 -0300

    Agregado el cuadrito de los hashtags mas nombrados. Falta ver como hacer para borrar el texto que dice que no hay hashtags en este periodo de tiempo

--- Buitter/src/main/java/it/itba/edu/ar/web/HomePage.java
+++ Buitter/src/main/java/it/itba/edu/ar/web/HomePage.java
@@ -1,24 +1,31 @@
 package it.itba.edu.ar.web;
 
 import it.itba.edu.ar.domain.buit.Buit;
+import it.itba.edu.ar.domain.buit.BuitRepo;
+import it.itba.edu.ar.domain.buit.Hashtag;
 import it.itba.edu.ar.domain.user.User;
 import it.itba.edu.ar.domain.user.UserRepo;
 import it.itba.edu.ar.web.base.BasePage;
+import it.itba.edu.ar.web.buit.HashtagPage;
 import it.itba.edu.ar.web.buit.ProfilePage;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 import org.apache.wicket.datetime.markup.html.basic.DateLabel;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
@@ -26,6 +33,12 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 public class HomePage extends BasePage {
 	@SpringBean	
 	private UserRepo userRepo;
+	@SpringBean	
+	private BuitRepo buitRepo;
+	private Integer time = 1;
+	private static final int trendingQuantity = 10;
+	private long DAY_IN_MS = 1000 * 60 * 60 * 24;
+
 	
 	public HomePage() {
 		final BuitterSession session = BuitterSession.get();
@@ -104,6 +117,56 @@ public class HomePage extends BasePage {
 			}	
 		});
 		
+		final IModel<List<Hashtag>> trendingTopicModel = new LoadableDetachableModel<List<Hashtag>>() {
+			@Override
+			protected List<Hashtag> load() {
+				Date tdate = new Date(System.currentTimeMillis() - (time * DAY_IN_MS));
+				return buitRepo.getHashtagsSinceDate(tdate,trendingQuantity);
+			}
+		};
+		add(new ListView<Hashtag>("hashtags", trendingTopicModel) {
+			@Override
+			protected void populateItem(ListItem<Hashtag> item) {
+				PageParameters pgParameters = new PageParameters();
+				pgParameters.add("hashtag", item.getModelObject().getHashtag());
+				BookmarkablePageLink<Void> hashtagPageLink = new BookmarkablePageLink<Void>("hashtagPageLink", HashtagPage.class, pgParameters);
+				hashtagPageLink.add(new Label("hashtag", new PropertyModel<String>(item.getModel(), "hashtag")));
+				hashtagPageLink.add(new Label("hashtagCount", new PropertyModel<String>(item.getModel(), "count")));
+				item.add(hashtagPageLink);
+			}
+		});
+		add(new Label("trendingEmpty", new Model<String>("There are no hashtags for this period of time.")).setVisible(trendingTopicModel.getObject().isEmpty()));
+		
+		final List<Integer> integers = Arrays.asList(new Integer[] {1,7,30});
+		IChoiceRenderer<Integer> renderer = new IChoiceRenderer<Integer>() {
+			@Override
+			public Object getDisplayValue(Integer key) {
+				switch(key) {
+					case 1: return "1 day";
+					case 7: return "1 week";
+					case 30: return "1 month";
+					default: throw new IllegalStateException(time + " is not mapped!");
+				}
+			}
+
+			@Override
+			public String getIdValue(Integer key, int index) {
+				return String.valueOf(integers.get(index));
+			}
+		};
+		DropDownChoice<Integer> dpdown = new DropDownChoice<Integer>("timeSelect", new PropertyModel<Integer>(this, "time"), integers, renderer) {
+			@Override
+			protected void onSelectionChanged(final Integer newSelection) {
+				super.onSelectionChanged(newSelection);
+				trendingTopicModel.detach();
+				time = newSelection;
+			}
+			@Override
+            protected boolean wantOnSelectionChangedNotifications() {
+                return true;
+            }
+		};
+		add(dpdown);
 		add(emptyUserContainer);
 		add(notEmptyUserContainer);
 	}
