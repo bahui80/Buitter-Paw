d1c6d36e7151c24ce3640c7623458d2826a81799
juan <bahui80@gmail.com>
2014-12-15 00:34:13 -0300

    Una cosa mejorada para que las imagenes no se cacheen

--- Buitter/src/main/java/it/itba/edu/ar/web/ImageResourceReference.java
+++ Buitter/src/main/java/it/itba/edu/ar/web/ImageResourceReference.java
@@ -2,23 +2,25 @@ package it.itba.edu.ar.web;
 
 import it.itba.edu.ar.domain.user.User;
 
+import java.util.Random;
+
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.resource.DynamicImageResource;
 import org.apache.wicket.request.resource.IResource;
 import org.apache.wicket.request.resource.ResourceReference;
-import org.apache.wicket.util.time.Time;
 
 public class ImageResourceReference extends ResourceReference {
 
 	private IModel<User> model;
 		
 	public ImageResourceReference(IModel<User> model) {
-		super(ImageResourceReference.class, "images" + model.getObject().getId());
+		super(ImageResourceReference.class, "images" + model.getObject().getId() + new Random().nextInt(100000));
 		this.model = model;
 	}
 	
 	@Override
 	public IResource getResource() {
+		model.detach();
 		if(model.getObject().getPhoto() == null) {
 			model.detach();
 			return BuitterApp.NO_IMAGE.getResource();
@@ -29,11 +31,6 @@ public class ImageResourceReference extends ResourceReference {
 			protected byte[] getImageData(Attributes attributes) {
 				model.detach();
 				return model.getObject().getPhoto();
-			}
-			
-			@Override
-			protected synchronized void setLastModifiedTime(Time time) {
-				super.setLastModifiedTime(Time.now());
 			}
 		};
 		
