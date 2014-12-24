77ae146f7fcb8991cfe69a7a044d82f18cc15e29
juan <bahui80@gmail.com>
2014-12-24 19:18:13 -0300

    Corregido lo del insert

--- Buitter/src/test/resources/insert3.sql
+++ Buitter/src/test/resources/insert3.sql
@@ -1,11 +1,11 @@
 -- BEGIN NEW INSERTS USERLIST --
-INSERT INTO userlist(id, description, name, owner_id) VALUES (1, 'Alumnos del ITBA', 'Alumnos', 6);
-INSERT INTO userlist(id, description, name, owner_id) VALUES (2, 'Profesores del ITBA', 'Profesores', 1);
-INSERT INTO userlist(id, description, name, owner_id) VALUES (3, 'Chicas de reggaeton', 'Reggaeton', 1);
-INSERT INTO userlist(id, description, name, owner_id) VALUES (4, 'Gente de la facu', 'ITBA', 1);
-INSERT INTO userlist(id, description, name, owner_id) VALUES (5, 'Gente de la facu', 'ITBA', 7);
-INSERT INTO userlist(id, description, name, owner_id) VALUES (6, 'Gente del colegio', 'CMB', 7);
-INSERT INTO userlist(id, description, name, owner_id) VALUES (7, 'Gente del gimnasio', 'Gimnasio', 7);
+INSERT INTO userlist(description, name, owner_id) VALUES ('Alumnos del ITBA', 'Alumnos', 6);
+INSERT INTO userlist(description, name, owner_id) VALUES ('Profesores del ITBA', 'Profesores', 1);
+INSERT INTO userlist(description, name, owner_id) VALUES ('Chicas de reggaeton', 'Reggaeton', 1);
+INSERT INTO userlist(description, name, owner_id) VALUES ('Gente de la facu', 'ITBA', 1);
+INSERT INTO userlist(description, name, owner_id) VALUES ('Gente de la facu', 'ITBA', 7);
+INSERT INTO userlist(description, name, owner_id) VALUES ('Gente del colegio', 'CMB', 7);
+INSERT INTO userlist(description, name, owner_id) VALUES ('Gente del gimnasio', 'Gimnasio', 7);
 -- END NEW INSERTS USERLIST --
 
 -- BEGIN NEW INSERTS USERLIST_USERS --
@@ -30,8 +30,8 @@ INSERT INTO users_blacklist(blacklist_id, blacklistedby_id) VALUES (3, 6);
 -- END NEW INSERTS USERS_BLACKLIST --
 
 -- BEGIN NEW INSERTS PUBLICITIES --
-INSERT INTO publicity(id, clientname, imageurl, url) VALUES (1, 'Bellevue', 'http://www.bellevuecollege.edu/ps/Images%202/MoE-Banner-12-10.jpg', 'http://www.bellevuecollege.edu/');
-INSERT INTO publicity(id, clientname, imageurl, url) VALUES (2, 'Three frogs graphics', 'http://www.treefrog-graphics.co.uk/images/Product%20Images/Banner%20Example.jpg', 'http://www.treefrog-graphics.co.uk/');
-INSERT INTO publicity(id, clientname, imageurl, url) VALUES (3, 'Frogger', 'http://wiki.thegamesdb.net/images/d/d7/Banner-example-3.jpg', 'http://www.happyhopper.org/welcome.php');
-INSERT INTO publicity(id, clientname, imageurl, url) VALUES (4, 'Aerolineas Argentinas', 'http://www.plusgrade.com/images/banner-example-1.jpg', 'http://www.aerolineas.com.ar/');
+INSERT INTO publicity(clientname, imageurl, url) VALUES ('Bellevue', 'http://www.bellevuecollege.edu/ps/Images%202/MoE-Banner-12-10.jpg', 'http://www.bellevuecollege.edu/');
+INSERT INTO publicity(clientname, imageurl, url) VALUES ('Three frogs graphics', 'http://www.treefrog-graphics.co.uk/images/Product%20Images/Banner%20Example.jpg', 'http://www.treefrog-graphics.co.uk/');
+INSERT INTO publicity(clientname, imageurl, url) VALUES ('Frogger', 'http://wiki.thegamesdb.net/images/d/d7/Banner-example-3.jpg', 'http://www.happyhopper.org/welcome.php');
+INSERT INTO publicity(clientname, imageurl, url) VALUES ('Aerolineas Argentinas', 'http://www.plusgrade.com/images/banner-example-1.jpg', 'http://www.aerolineas.com.ar/');
 -- END NEW INSERTS PUBLICITIES --
